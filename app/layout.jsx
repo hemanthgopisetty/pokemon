e543b682bd2208ac2c42cedaec6c711738d4a1d3
hemanthgopisetty <hemanth.508136@gmail.com>
2024-07-07 22:36:58 +0530

    google

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <p>https://github.com/hemanthgopisetty/pokemon.git</p>
+        <p className='opacity-0'>google-site-verification: googleeda34052c8fdb401.html</p>
         <ThemeProvider attribute="class" defaultTheme="dark">
         <main className="flex min-h-screen flex-col items-center p-24">
               <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
